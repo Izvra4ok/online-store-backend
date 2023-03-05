13f2a3208969ea1bf0e3fa68ba875ff717c8a7ca
Izvra4ok <barzakouski.sergey@gmail.com>
2023-03-05 12:54:04 +0300

    add database diagram

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Serve   r started on port ${PORT}`))
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log("Unable to connect to the database",e)
     }
